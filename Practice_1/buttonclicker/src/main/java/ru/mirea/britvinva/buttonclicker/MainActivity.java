02ba78ad26077da21bc6a24015ed5b1877d5be19
vlad <vladislave.britvin@yandex.ru>
2026-02-25 22:45:17 +0300

    практика 1

---
+++ Practice_1/buttonclicker/src/main/java/ru/mirea/britvinva/buttonclicker/MainActivity.java
@@ -0,0 +1,56 @@
+package ru.mirea.britvinva.buttonclicker;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textViewStudent;
+    private Button btnWhoAmI;
+    private Button btnItIsNotMe;
+
+    private CheckBox checkBox;
+
+    public void onMyButtonClick(View view)
+    {
+        textViewStudent.setText("ЭТО НЕ Я!!!");
+        Toast.makeText(this,
+                "Ещё один способ!", Toast.LENGTH_SHORT).show();
+        checkBox.toggle();
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        textViewStudent = findViewById(R.id.tvOut);
+        btnWhoAmI = findViewById(R.id.btnWhoAmI);
+        btnItIsNotMe = findViewById(R.id.btnItIsNotMe);
+        checkBox = findViewById(R.id.MycheckBox);
+
+        View.OnClickListener oclBtnWhoAmI = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textViewStudent.setText("Мой номер по списку № 3");
+                checkBox.toggle();
+            }
+        };
+        btnWhoAmI.setOnClickListener(oclBtnWhoAmI);
+
+    }
+}
---
+++ Practice_1/settings.gradle.kts
@@ -0,0 +1,28 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Project1"
+include(":app")
+include(":control_lesson1")
+include(":buttonclicker")
