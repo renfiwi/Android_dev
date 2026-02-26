06f3239d98be6db7fb9ce3d5a53749ff2721ca19
vlad <vladislave.britvin@yandex.ru>
2026-02-26 10:04:49 +0300

    практика1.1

--- Practice_1/app/src/main/java/ru/mirea/BritvinVA/lesson1/MainActivity.java
+++ Practice_1/app/src/main/java/ru/mirea/BritvinVA/lesson1/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.homepage_layout);
+        setContentView(R.layout.liner);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.notmain), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
